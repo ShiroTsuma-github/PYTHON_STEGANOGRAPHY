5a3c4c5e1288b7cef8eed39f5365562f5d33d507
Hubinix52-9 <hubert.przewozniak.2001@gmail.com>
2023-08-04 17:22:09 +0200

    added buttons, labels and text fields

--- src/GUI.py
+++ src/GUI.py
@@ -16,6 +16,8 @@ def decoding():
 def getting_image():
     print("Image downloaded")
 
+def creating_encoded_key():
+    print("Key was created")
 
 root = tk.Tk()
 root.title("Stenography App")
@@ -37,33 +39,46 @@ root.resizable(False, False)
 photo_frame = tk.LabelFrame(root, bd=3, relief=tk.GROOVE, width=400, height=300)
 photo_frame.grid(row=0, column=0, padx=5, pady=5)
 
-buttons_frame = tk.LabelFrame(root, bd=3, relief=tk.GROOVE, width=210, height=300)
-buttons_frame.grid(row=0, column=1, padx=5, pady=5)
+buttons_frame = tk.LabelFrame(root, bd=3, relief=tk.GROOVE, width=200, height=300)
+buttons_frame.grid(row=0, column=1, pady=5)
 
 input_output_frame = tk.LabelFrame(root, bd=3, relief=tk.GROOVE, width=620, height=150)
 input_output_frame.grid(row=1, column=0, columnspan=2, padx=10, pady=5)
 
-#encode_button = tk.Button(buttons_frame, text="Encode", command=encoding)
-#encode_button.pack()
+encode_button = tk.Button(buttons_frame, text="Encode", command=encoding, width=28, height=4)
+encode_button.grid(row=0, column=0, columnspan=2, pady=20, padx=10)
 
-#decode_button = tk.Button(buttons_frame, text="Decode", command=decoding)
-#decode_button.pack()
+decode_button = tk.Button(buttons_frame, text="Decode", command=decoding, width=28, height=4)
+decode_button.grid(row=1, column=0, columnspan=2, pady=20, padx=10)
 
-#key_crypting_text_field = tk.Entry(buttons_frame)
-#key_crypting_text_field.pack()
+key_crypting_text_field = tk.Text(buttons_frame, width=18, height=2)
+key_crypting_text_field.grid(row=2, column=0, pady=20)
 
-#input_text_field = tk.Entry(input_output_frame, width=200)
-#input_text_field.pack(padx=10, pady=10)
+image2 = Image.open(f"{ROOT_DIR}/resources/images/randomize.png")
+new_image2 = image2.resize((28, 28))
+test2 = ImageTk.PhotoImage(new_image2)
 
-#output_text_field = tk.Entry(input_output_frame, width=300)
-#output_text_field.pack(padx=10, pady=10)
+creating_key_button = tk.Button(buttons_frame, width=28, height=28, image=test2, command=creating_encoded_key)
+creating_key_button.grid(row=2, column=1)
 
-image1 = Image.open(f"{ROOT_DIR}/resources/images/img1.jpeg")
-new_image = image1.resize((400, 300))
+label_for_input_field = tk.Label(input_output_frame, text="Input data:")
+label_for_input_field.grid(row=0, padx=10, pady=2)
+
+input_text_field = tk.Text(input_output_frame, width=75, height=2)
+input_text_field.grid(row=1, padx=10, pady=5)
+
+label_for_output_field = tk.Label(input_output_frame, text="Output data:")
+label_for_output_field.grid(row=2, padx=10, pady=2)
+
+output_text_field = tk.Text(input_output_frame, width=75, height=2)
+output_text_field.grid(row=3, padx=10, pady=5)
+
+image1 = Image.open(f"{ROOT_DIR}/resources/images/png2.png")
+new_image = image1.resize((370, 290))
 test = ImageTk.PhotoImage(new_image)
 
-image_label = tk.Label(photo_frame, image=test)
-image_label.grid(row=0, column=0, sticky="nsew")
+image_getting_button = tk.Button(photo_frame, image=test, command=getting_image)
+image_getting_button.grid(row=0, column=0, sticky="nsew")
 
 
 
