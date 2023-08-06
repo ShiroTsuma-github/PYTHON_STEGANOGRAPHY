57440d9c8451ef2d7526d49f1acf2953b06f3a63
ShiroTsuma-github <spam.shiro@gmail.com>
2023-08-06 13:14:48 +0200

    changed obfuscation to maybe fix? Co-authored-by: Hubinix52-9 <Hubinix52-9@users.noreply.github.com>

--- src/obfuscateText.py
+++ src/obfuscateText.py
@@ -51,26 +51,19 @@ class TextObfuscator():
         coded_message = ''
         segment_pointer = 0
         message_pointer = 0
-        offset = 0
         if key is None:
             self.__key: str = next(self.__key_generator)
         else:
             self.__key: str = key
-        key_binary: List[str] = self.__char_encoder.string_to_bits(self.__key)
+        key_binary: List[str] = list(reversed(self.__char_encoder.string_to_bits(self.__key)))
         key_nums: list[int] = [int(char, 16) for char in self.__key]
         while True:
             coded_char: str = self.__xor(bin_message[message_pointer], reversed(key_binary[segment_pointer]))
             coded_message += coded_char
-            if key_nums[segment_pointer] % 2 == 0:
-                offset = 1
-            else:
-                offset = 2
-            if segment_pointer + offset >= len(key_nums):
+            if segment_pointer + 1 >= len(key_nums):
                 segment_pointer = 0
             message_pointer += 1
             segment_pointer += 1
-            if segment_pointer >= len(key_nums):
-                segment_pointer = 0
             if message_pointer >= len(text):
                 break
         return (coded_message, self.__key)
@@ -81,22 +74,18 @@ class TextObfuscator():
         decoded_message = ''
         segment_pointer = 0
         message_pointer = 0
-        offset = 0
-        key_binary: List[str] = self.__char_encoder.string_to_bits(key)
+        key_binary: List[str] = list(reversed(self.__char_encoder.string_to_bits(key)))
         key_nums: list[int] = [int(char, 16) for char in key]
         while True:
             decoded_char: str = self.__xor(bin_message[message_pointer], reversed(key_binary[segment_pointer]))
-            decoded_message += self.__char_encoder.bits_to_char(decoded_char)
-            if key_nums[segment_pointer] % 2 == 0:
-                offset = 1
-            else:
-                offset = 2
-            if segment_pointer + offset >= len(key_nums):
+            try:
+                decoded_message += self.__char_encoder.bits_to_char(decoded_char)
+            except ValueError:
+                decoded_message += ' '
+            if segment_pointer + 1 >= len(key_nums):
                 segment_pointer = 0
             message_pointer += 1
             segment_pointer += 1
-            if segment_pointer >= len(key_nums):
-                segment_pointer = 0
             if message_pointer >= len(bin_message):
                 break
         return decoded_message
@@ -115,12 +104,7 @@ class TextObfuscator():
 
 if __name__ == "__main__":
     to = TextObfuscator()
-    # print(to.obfuscate('test'))
-    # for i in range(10):
-    #     print(to.obfuscate('Co tam u ciebie Adamie? Bo u mnie świetnie'))
     mess, key = to.obfuscate('Co tam u ciebie   Adamie? Bo u mnie świetnie')
     print(mess, key)
     print(to.coded_message_to_string(mess))
     print(to.deobfuscate(mess, key))
-
-# '0110101 0100001 0110011 0110101'
