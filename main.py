6be96fccf9589b1bbfa9efeb32c6a82a48c08a8a
ShiroTsuma-github <spam.shiro@gmail.com>
2023-08-09 13:45:43 +0200

    final app and app building

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-a = 32
+import sys
+import os
 
 
+ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(ROOT_DIR)
+from src.GUI import App     # noqa: E402
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
--- src/Untitled-1.py
+++
@@ -1,5 +0,0 @@
-plik = ''
-for x in range(200):
-    plik += str(x) + ',' +  str(x) + ',' +  str(x)
-    plik += '\n'
-print(plik)
